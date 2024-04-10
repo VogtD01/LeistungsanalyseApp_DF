b67d859ad355df0934073d5516d5d4c3bd6bf512
VogtD01 <vd6806@mci4me.at>
2024-04-10 13:28:09 +0200

    first commit

---
+++ calculation.py
@@ -0,0 +1,6 @@
+# calculation.py
+def calc_mean(list_of_measurements : list[int]):
+  """ Eine Funktion, die eine Liste von Werten übernimmt und das arithmetische Mittel zurück gibt"""
+
+  mittelwert = sum(list_of_measurements) / len(list_of_measurements)
+  return mittelwert
---
+++ debug.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    sum = a + b
+    return sum    
+
+
+print("Hello, World!")
+x = 1
+y = 5
+x = x / y
+
+z = add(x,y)
+
+print(z)
+
+
+
+
+import numpy as np
+
+a = np.array([1,2,3,4,5])
